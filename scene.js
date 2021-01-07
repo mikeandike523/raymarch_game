f4fb3ef437a7a26b93bdd3d149db03e9406c2847
Michael Sohnen <michaelsohnenacademic@gmail.com>
2021-01-06 23:48:40 -0500

    finished orbit camera, not sure why something about the orbit was funny untila  random fi

--- scene.js
+++ scene.js
@@ -1,6 +1,10 @@
+var auxillaryFunctions=`
+`;
+
+
 var sceneDistanceScript=`
-    vec3 pa=vec3(p.x,p.y-0.2,p.z);
-    vec3 q = abs(pa) - vec3(0.4,0.4,0.4);
+    vec3 pa=vec3(p.x,p.y,p.z);
+    vec3 q = abs(pa) - vec3(0.5,0.5,0.5);
     float dist1= length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
   return dist1;
 `;
@@ -8,5 +12,16 @@ var sceneDistanceScript=`
 var shadingScript=`
 
    // return vec3(dot(n,normalize(vec3(1.,1.,1.))));
-   return abs(n);
+   vec3 baseColor=abs(n);
+   float condition= dot(n,normalize(vec3(1.,1.,1.)));
+   if(condition<0.){
+		float brick = 1.;
+		if(mod(floor(pt.x*10.)+floor(pt.y*10.)+floor(pt.z*10.),2.)<0.0001)
+		{
+			brick=0.6;
+		}
+		return baseColor*brick;
+
+   }
+   return baseColor;
 `;
