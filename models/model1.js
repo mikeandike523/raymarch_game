8eb8e256c46f24f19b70577fa2a43bc87d13eeb4
Michael Sohnen <michaelsohnenacademic@gmail.com>
2021-01-08 15:19:45 -0500

    some transform stuff

--- models/model1.js
+++ models/model1.js
@@ -1,22 +1,27 @@
 var auxillaryFunctions=`
-float atan2( float y, float x)
-{
-    return x == 0.0 ? sign(y)*PI/2. : atan(y, x);
-}
+
 float brick(vec3 v,float s){
 	if(mod(floor(v.x*s)+floor(v.y*s)+floor(v.z*s),2.)<0.001)
 		return 1.;
 	return 0.;
 }
+
+
+
+
+
 `;
 
 
 var sceneDistanceScript=`
-float dist = length(vec3(floor(pt.x*10.)/10.,pt.yz))-0.5;
+vec3 newPt=worldToObject(pt,vec3(0.5*sin(elapsedTime/500.),0.,0.),mat3(1.,0.,0.,0.,1.,0.,0.,0.,1.));
+float dist = length(vec3(floor(newPt.x*10.)/10.,newPt.yz))-0.5;
 return dist/20.;
 `;
 
 var shadingScript=`
-	vec3 baseColor = vec3(brick(pt,3.)*0.7+0.2,0.2,0.4);
+vec3 newPt=worldToObject(pt,vec3(0.5*sin(elapsedTime/500.),0.,0.),mat3(1.,0.,0.,0.,1.,0.,0.,0.,1.));
+	vec3 baseColor = vec3(brick(newPt,3.)*0.7+0.2,0.2,0.4);
 	return ((dot(n,normalize(vec3(1.,1.,1.)))/1.7+0.8)*0.95+0.05)*baseColor;
 `;
+coordinateAxes={}
