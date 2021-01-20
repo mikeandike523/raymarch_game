bfd10d777d1a854719b19b4a8d90e8cc1d72287a
Michael Sohnen <michaelsohnenacademic@gmail.com>
2021-01-20 14:41:38 -0500

    a

--- models/model1.js
+++ models/model1.js
@@ -14,14 +14,11 @@ float brick(vec3 v,float s){
 
 
 var sceneDistanceScript=`
-vec3 newPt=worldToObject(pt,vec3(0.5*sin(elapsedTime/500.),0.,0.),mat3(1.,0.,0.,0.,1.,0.,0.,0.,1.));
-float dist = length(vec3(floor(newPt.x*10.)/10.,newPt.yz))-0.5;
-return dist/20.;
+
+return length(pt)-0.5;
 `;
 
 var shadingScript=`
-vec3 newPt=worldToObject(pt,vec3(0.5*sin(elapsedTime/500.),0.,0.),mat3(1.,0.,0.,0.,1.,0.,0.,0.,1.));
-	vec3 baseColor = vec3(brick(newPt,3.)*0.7+0.2,0.2,0.4);
-	return ((dot(n,normalize(vec3(1.,1.,1.)))/1.7+0.8)*0.95+0.05)*baseColor;
+
+	return vec3(dot(n,vec3(0.,1.,0.)));
 `;
-coordinateAxes={}
