b7eef2648cdf68a49ce1491d2b6cb6faf43ef65d
Michael Sohnen <michaelsohnenacademic@gmail.com>
2021-02-24 03:25:59 -0500

    added something

--- models/model1.js
+++ models/model1.js
@@ -21,8 +21,8 @@ vec3 q = abs(pt) - b;
 float boundingBox= length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0); 
 float cylinder = length(pt.xz)-0.5*sin(pt.y)*sin(pt.y)-0.15;
 float ball = length(pt-vec3(0.,1.1,0.))-0.3;
-
-return min(max(cylinder,boundingBox),ball)/20.;
+float ground = pt.y+1.;
+return min(min(max(cylinder,boundingBox),ball),max(ground,boundingBox));
 `;
 var shadingScript=`
 	return abs(n);
